e2efc4f4f1530cb31d6a2fa87c51e6524d73507b
Faris Iqbal <faris.iqbal93@gmail.com>
2023-11-20 13:20:18 +0100

    Update ExperimentTrackerExample

--- app/src/main/java/com/example/asyncload/experiment/ExperimentTrackerExample.kt
+++ app/src/main/java/com/example/asyncload/experiment/ExperimentTrackerExample.kt
@@ -2,21 +2,13 @@ package com.example.asyncload.experiment
 
 import com.example.asyncload.experiment.provider.CustomerUserProvider
 import com.example.asyncload.experiment.provider.ExperimentationProvider
-import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.combine
 
 class ExperimentTrackerExample(
     private val experimentationProvider: ExperimentationProvider,
     private val customerUserProvider: CustomerUserProvider
-) : CustomerExperimentTracker(experimentationProvider, customerUserProvider){
+) : CustomerExperimentTracker(experimentationProvider, customerUserProvider) {
+    
     override val key: String = "my_tracking_key"
-
-    override val isReady: Flow<Boolean> = combine(
-        experimentationProvider.isReady,
-        customerUserProvider.data
-    ) { isExperimentationReady, data ->
-        isExperimentationReady && data.isNotEmpty()
-    }
 
     override suspend fun sendTrackingEvent() {
         experimentationProvider.track(key, customerUserProvider.data.value, userAttrs = mapOf())
